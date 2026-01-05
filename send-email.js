cbaa00f4138a626c09ed1f7b873b4cdf691c2f83
vs5jwnpdzn-coder <vs5jwnpdzn@privaterelay.appleid.com>
2026-01-05 13:57:56 +0100

    w

--- send-email.js
+++ send-email.js
@@ -5,7 +5,7 @@ const html = fs.readFileSync("./mail.html", "utf8");
 
 // OUTLOOK DATEN
 const OUTLOOK_USER = "paypaI.benz.login@outlook.com"; // z.B. name@outlook.de
-const OUTLOOK_APP_PASS = "lpjqsvcgmspniieh";
+const OUTLOOK_APP_PASS = "ullocxwtzyewbqqd";
 const TO = "jonasbenz25@gmail.com"; // zum Test auch deine eigene Adresse ok
 
 async function main() {
@@ -14,10 +14,13 @@ async function main() {
     port: 587,
     secure: false,
     auth: {
-      user: "paypaI.benz.login@outlook.com",
-      pass: "lpjqsvcgmspniieh",
+      user: OUTLOOK_USER,
+      pass: OUTLOOK_APP_PASS,
     },
   });
+// Testet Login + Verbindung (sehr hilfreich)
+  await transporter.verify();
+  console.log("SMTP Login OK");
 
   const info = await transporter.sendMail({
     from: `"Thomas" <${OUTLOOK_USER}>`,
